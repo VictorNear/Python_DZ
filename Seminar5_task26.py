f183e614bf55ee14310dc9049d59f515749ddb67
V.Nearonsky <v.nearonsky@yandex.ru>
2023-04-23 22:07:33 +0300

    Решение задачи 26

---
+++ Seminar5_task26.py
@@ -0,0 +1,16 @@
+# Задача 26:  Напишите программу, которая на вход принимает два числа A и B, и возводит число А 
+# в целую степень B с помощью рекурсии.
+# *Пример:*
+# A = 3; B = 5 -> 243 (3⁵)
+#     A = 2; B = 3 -> 8 
+
+number = int(input("Введите число: "))
+degree = int(input("Введите степень: "))
+res = number
+def degree_of(number, degree, res):
+   if degree == 1:
+      return res
+   else:
+      res = res * number
+      return degree_of(number, degree - 1, res)
+print(f"{number} в степени {degree} равно {degree_of(number, degree, res)}")
